3ee9a0f2e4ece4b5fd5acbf51edccea7410c091e
Christoph Grabo <asaaki@mannaz.cc>
2021-12-01 21:15:25 +0100

    Refactor day 1
    - use macro
    - use input helper
    - filter+count over fold

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,37 +1,18 @@
-use super::AnyDay;
-use crate::utils::*;
-
-pub(crate) struct Day {
-    pub(super) input: Vec<String>,
-}
-
-impl AnyDay for Day {
+aoc_macros::day_impl! {
     fn step1(&self) -> StringResult {
-        let depths: &Vec<usize> = &self
-            .input
-            .iter()
-            .map(|s| s.parse::<usize>().unwrap())
-            .collect();
+        let depths: Vec<usize> = s2t(&self.input);
 
-        let result: usize = depths
-            .windows(2)
-            .fold(0, |a, t| if t[1] > t[0] { a + 1 } else { a });
+        let result: usize = depths.windows(2).filter(|&t| t[0] < t[1]).count();
 
         Ok(format!("{}", result))
-    }
+    },
 
     fn step2(&self) -> StringResult {
-        let depths: &Vec<usize> = &self
-            .input
-            .iter()
-            .map(|s| s.parse::<usize>().unwrap())
-            .collect();
-
+        let depths: Vec<usize> = s2t(&self.input);
+        // due to .windows not available on iterators, we have to take this midstate first
         let sums: Vec<usize> = depths.windows(3).map(|w| w.iter().sum()).collect();
-
-        let result: usize = sums
-            .windows(2)
-            .fold(0, |a, t| if t[1] > t[0] { a + 1 } else { a });
+        // … to then .windows and .filter again
+        let result: usize = sums.windows(2).filter(|&t| t[0] < t[1]).count();
 
         Ok(format!("{}", result))
     }
