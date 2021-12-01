9f96f934d58566afd0c21e03b172223c883fccb0
Christoph Grabo <asaaki@mannaz.cc>
2021-12-01 19:28:51 +0100

    Move testing to call_a_day

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -2,20 +2,17 @@ use super::AnyDay;
 use crate::utils::*;
 
 pub(crate) struct Day {
-    pub(super) test: Option<String>,
     pub(super) input: Vec<String>,
 }
 
 impl AnyDay for Day {
     fn step1(&self) -> StringResult {
         dbg!(&self.input);
-        dbg!(&self.test);
         Ok("[step1] not implemented yet".into())
     }
 
     fn step2(&self) -> StringResult {
         dbg!(&self.input);
-        dbg!(&self.test);
         Ok("[step2] not implemented yet".into())
     }
 }
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -47,37 +47,41 @@ pub(crate) fn call_a_day(
     test: Option<String>,
 ) -> StringResult {
     let maybe_day: Option<Box<dyn AnyDay>> = match day {
-        0 => Some(Box::new(day0::Day { input, test })),
-        1 => Some(Box::new(day1::Day { input, test })),
-        2 => Some(Box::new(day2::Day { input, test })),
-        3 => Some(Box::new(day3::Day { input, test })),
-        4 => Some(Box::new(day4::Day { input, test })),
-        5 => Some(Box::new(day5::Day { input, test })),
-        6 => Some(Box::new(day6::Day { input, test })),
-        7 => Some(Box::new(day7::Day { input, test })),
-        8 => Some(Box::new(day8::Day { input, test })),
-        9 => Some(Box::new(day9::Day { input, test })),
-        10 => Some(Box::new(day10::Day { input, test })),
-        11 => Some(Box::new(day11::Day { input, test })),
-        12 => Some(Box::new(day12::Day { input, test })),
-        13 => Some(Box::new(day13::Day { input, test })),
-        14 => Some(Box::new(day14::Day { input, test })),
-        15 => Some(Box::new(day15::Day { input, test })),
-        16 => Some(Box::new(day16::Day { input, test })),
-        17 => Some(Box::new(day17::Day { input, test })),
-        18 => Some(Box::new(day18::Day { input, test })),
-        19 => Some(Box::new(day19::Day { input, test })),
-        20 => Some(Box::new(day20::Day { input, test })),
-        21 => Some(Box::new(day21::Day { input, test })),
-        22 => Some(Box::new(day22::Day { input, test })),
-        23 => Some(Box::new(day23::Day { input, test })),
-        24 => Some(Box::new(day24::Day { input, test })),
-        25 => Some(Box::new(day25::Day { input, test })),
+        0 => Some(Box::new(day0::Day { input })),
+        1 => Some(Box::new(day1::Day { input })),
+        2 => Some(Box::new(day2::Day { input })),
+        3 => Some(Box::new(day3::Day { input })),
+        4 => Some(Box::new(day4::Day { input })),
+        5 => Some(Box::new(day5::Day { input })),
+        6 => Some(Box::new(day6::Day { input })),
+        7 => Some(Box::new(day7::Day { input })),
+        8 => Some(Box::new(day8::Day { input })),
+        9 => Some(Box::new(day9::Day { input })),
+        10 => Some(Box::new(day10::Day { input })),
+        11 => Some(Box::new(day11::Day { input })),
+        12 => Some(Box::new(day12::Day { input })),
+        13 => Some(Box::new(day13::Day { input })),
+        14 => Some(Box::new(day14::Day { input })),
+        15 => Some(Box::new(day15::Day { input })),
+        16 => Some(Box::new(day16::Day { input })),
+        17 => Some(Box::new(day17::Day { input })),
+        18 => Some(Box::new(day18::Day { input })),
+        19 => Some(Box::new(day19::Day { input })),
+        20 => Some(Box::new(day20::Day { input })),
+        21 => Some(Box::new(day21::Day { input })),
+        22 => Some(Box::new(day22::Day { input })),
+        23 => Some(Box::new(day23::Day { input })),
+        24 => Some(Box::new(day24::Day { input })),
+        25 => Some(Box::new(day25::Day { input })),
         _ => None,
     };
 
     match maybe_day {
-        Some(d) => d.step(step),
+        Some(d) => {
+            let result = d.step(step)?;
+            test.map(|expected| assert_eq!(result, expected));
+            Ok(result)
+        }
         None => Err(CustomError("invalid day".into())),
     }
 }
