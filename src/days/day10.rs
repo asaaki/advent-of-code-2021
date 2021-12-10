d212ba8177dedb66e8c57d66ad606fc1c018b4a9
Christoph Grabo <asaaki@mannaz.cc>
2021-12-10 20:16:51 +0100

    Add solution for day 10

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,9 +1,72 @@
+const TAGS: [(char, char); 4] =
+    [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')];
+
+const TAG_VALUES: [usize; 4] = [3, 57, 1197, 25137];
+
 aoc_macros::day_impl! {
     fn part1(&self) -> StringResult {
-        ok_string("[step1] not implemented yet")
+        let result = compute(self.input, true);
+        ok_string(result)
     },
 
     fn part2(&self) -> StringResult {
-        ok_string("[step2] not implemented yet")
+        let result = compute(self.input, false);
+        ok_string(result)
     }
+}
+
+fn compute(input: StrInputRef, summarize_corrupted: bool) -> usize {
+    let len = maxlen(input);
+    let mut stack = Vec::with_capacity(len);
+    if summarize_corrupted {
+        input
+            .iter()
+            .fold(0, |acc, line| acc + validate(line, &mut stack))
+    } else {
+        let mut scores: Vec<usize> = input
+            .iter()
+            .filter_map(|line| {
+                (validate(line, &mut stack) == 0).then(|| {
+                    stack.iter().rev().fold(0, |acc, c| {
+                        (acc * 5)
+                            + TAGS.iter().position(|(_, t)| t == c).unwrap()
+                            + 1
+                    })
+                })
+            })
+            .collect();
+        scores.sort_unstable();
+        scores[scores.len() / 2]
+    }
+}
+
+// to reserve the minimum max capacity necessary
+// worst case scenario: a full list of opening tags
+#[inline]
+fn maxlen(input: StrInputRef) -> usize {
+    input
+        .iter()
+        .map(|l| l.len())
+        .max_by(|l1, l2| l1.cmp(l2))
+        .unwrap_or(128)
+}
+
+// #[inline]
+fn validate(line: &str, stack: &mut Vec<char>) -> usize {
+    stack.clear();
+    for c in line.chars() {
+        for (idx, (open, close)) in TAGS.iter().enumerate() {
+            if c == *open {
+                stack.push(*close);
+            }
+            if c == *close {
+                if stack.last().unwrap() == &c {
+                    stack.pop();
+                } else {
+                    return TAG_VALUES[idx];
+                }
+            }
+        }
+    }
+    0
 }
