dc9d335896250915c954a7689bed783a5837fd43
Christoph Grabo <asaaki@mannaz.cc>
2021-12-12 18:48:55 +0100

    Simplify day 12
    Thanks to @hwartig for his improved solution as an inspiration:
    https://github.com/hwartig/adventofcode.js/blob/3d907ed/12/index.mjs
    
    This is also more efficient on Rust apparently.
    
    - debug mode: 5s -> 880ms (5.7 times faster)
    - release mode: 337ms -> 39ms (8.6 times faster)

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,80 +1,67 @@
-use std::{collections::HashMap, ops::AddAssign};
+use std::collections::HashMap;
 
 aoc_macros::day_impl_common!();
 
 type Id<'a> = &'a str;
 type Nodes<'a> = Vec<Id<'a>>;
 type CaveSystem<'a> = HashMap<Id<'a>, Nodes<'a>>;
-type Visits<'a> = HashMap<Id<'a>, u8>;
+type CavePath<'a> = Vec<Id<'a>>;
 
 fn compute(input: StrInputRef, first_part: bool) -> usize {
-    let (caves, visits) = make_maps(input);
-    let mut counts = 0;
-    traverse_cave(first_part, &caves, visits, "start", &mut counts);
-    counts
+    traverse_cave(!first_part, &make_caves(input), "start", vec!["start"])
 }
 
-fn make_maps(input: StrInputRef) -> (CaveSystem, Visits) {
+#[inline]
+fn make_caves(input: StrInputRef) -> CaveSystem {
     let mut caves = CaveSystem::new();
-    let mut visits = Visits::new();
-
     for line in input {
         let (l, r) = line.split_once("-").unwrap();
-
         let entry = caves.entry(&l).or_insert(Nodes::new());
         entry.push(&r);
         let entry = caves.entry(&r).or_insert(Nodes::new());
         entry.push(&l);
     }
-
-    for node in caves.keys() {
-        if node.chars().all(|c| c.is_ascii_lowercase()) {
-            visits.insert(node, 0);
-        }
-    }
-
-    (caves, visits)
+    caves
 }
 
 fn traverse_cave(
-    first_part: bool,
+    twice: bool,
     caves: &CaveSystem,
-    visits: Visits,
-    id: &str,
-    counts: &mut usize,
-) -> () {
-    let mut visits = visits;
-
-    if id.chars().all(|c| c.is_ascii_lowercase()) {
-        let &c = visits.get(id).unwrap();
-        if first_part {
-            if c > 0 {
-                return;
-            } else {
-                visits.get_mut(id).unwrap().add_assign(1);
-            }
-        } else {
-            if id == "start" && visits.iter().any(|(_, &v)| v > 0) {
-                return;
-            }
-
-            if c > 0 {
-                if visits.iter().all(|(_, &v)| v < 2) {
-                    visits.get_mut(id).unwrap().add_assign(1);
-                } else {
-                    return;
-                }
-            } else {
-                visits.get_mut(id).unwrap().add_assign(1);
-            }
+    id: Id,
+    path: CavePath,
+) -> usize {
+    caves.get(id).unwrap().iter().fold(0, |count, &node| {
+        if node == "start" {
+            return count;
         }
-    }
-
-    for &cave in caves.get(id).unwrap() {
-        if cave == "end" {
-            counts.add_assign(1);
-        } else {
-            traverse_cave(first_part, caves, visits.clone(), cave, counts);
+        if node == "end" {
+            return count + 1;
         }
-    }
+        if is_lower(node) && path.contains(&node) {
+            if twice {
+                return count
+                    + traverse_cave(
+                        false,
+                        caves,
+                        node,
+                        new_path_with(&node, &path),
+                    );
+            }
+            return count;
+        }
+
+        count + traverse_cave(twice, caves, node, new_path_with(&node, &path))
+    })
+}
+
+#[inline]
+fn is_lower(id: Id) -> bool {
+    id.chars().all(|c| c.is_ascii_lowercase())
+}
+
+#[inline]
+fn new_path_with<'a>(node: &'a Id, path: &'a CavePath) -> CavePath<'a> {
+    let mut new_path = path.clone();
+    new_path.push(node);
+    new_path
 }
