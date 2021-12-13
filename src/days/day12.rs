7456a88578c94b144bbd576fa6aeb0656c0979b7
Christoph Grabo <asaaki@mannaz.cc>
2021-12-13 01:14:01 +0100

    Improve performance even further
    Collected some inspiration from the AoC subreddit.
    - switch (back) to a Node enum, but with u32 as data for Small/Big
    - use ahash instead of std's hashmap
    - use hash32 for node data creation
    
    Not all things add a lot of improvements, the hash32/u32 was an attempt
    to reduce the size of the enum (with u64:16 bytes, u32 only 8 bytes),
    but it gains only around 1ms max (probably less).
    Also ahash was not a very huge deal on its own.
    But the combination of all the things got me 11ms over previous version.
    
    Performance results:
    - debug mode: 5s -> 514ms (9.7 times faster)
    - release mode: 337ms -> 28ms (12 times faster)

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,18 +1,57 @@
-use std::collections::HashMap;
+// even though it's said that default HS also uses aHash,
+// using AHashMap directly is still faster
+use ahash::AHashMap as Map;
 
-type Id<'a> = &'a str;
-type Nodes<'a> = Vec<Id<'a>>;
-type CaveSystem<'a> = HashMap<Id<'a>, Nodes<'a>>;
-type CavePath<'a> = Vec<Id<'a>>;
-type CavePathRef<'a> = &'a [Id<'a>];
+// 64 bit Node data
+// use ahash::AHasher as NodeDataHasher;
+// use std::hash::{Hash, Hasher};
+// type NodeData = u64;
 
-const START: &str = "start";
-const END: &str = "end";
+// use a 32bit hash for the node data, so the enum becomes smaller
+// performance difference to 64 bits is in the microseconds though
+use hash32::{FnvHasher as NodeDataHasher, Hash, Hasher};
+type NodeData = u32;
+
+type Nodes = Vec<Node>;
+type CaveSystem<'a> = Map<Node, Nodes>;
+type CavePath<'a> = Vec<&'a Node>;
+type CavePathRef<'a> = &'a [&'a Node];
+
+#[derive(Hash, PartialEq, Eq, Debug, Clone, Copy)]
+enum Node {
+    Start,
+    End,
+    Small(NodeData),
+    Big(NodeData),
+}
+
+impl Node {
+    fn new(s: &str) -> Self {
+        if s == "start" {
+            Node::Start
+        } else if s == "end" {
+            Node::End
+        } else if s.chars().next().unwrap().is_lowercase() {
+            let mut hs = NodeDataHasher::default();
+            s.hash(&mut hs);
+            Node::Small(hs.finish())
+        } else {
+            let mut hs = NodeDataHasher::default();
+            s.hash(&mut hs);
+            Node::Big(hs.finish())
+        }
+    }
+}
 
 aoc_macros::day_impl_common!();
 
 fn compute(input: StrInputRef, first_part: bool) -> usize {
-    traverse_cave(!first_part, &make_caves(input), START, vec![START])
+    traverse_cave(
+        !first_part,
+        &make_caves(input),
+        &Node::Start,
+        vec![&Node::Start],
+    )
 }
 
 #[inline]
@@ -20,6 +59,7 @@ fn make_caves(input: StrInputRef) -> CaveSystem {
     let mut caves = CaveSystem::new();
     for line in input {
         let (l, r) = line.split_once("-").unwrap();
+        let (l, r) = (Node::new(l), Node::new(r));
         let entry = caves.entry(l).or_insert_with(Nodes::new);
         entry.push(r);
         let entry = caves.entry(r).or_insert_with(Nodes::new);
@@ -31,40 +71,42 @@ fn make_caves(input: StrInputRef) -> CaveSystem {
 fn traverse_cave(
     twice: bool,
     caves: &CaveSystem,
-    id: Id,
+    current: &Node,
     path: CavePath,
 ) -> usize {
-    caves.get(id).unwrap().iter().fold(0, |count, &node| {
-        if node == START {
-            return count;
-        }
-        if node == END {
-            return count + 1;
-        }
-        if is_lower(node) && path.contains(&node) {
-            if twice {
-                return count
-                    + traverse_cave(
-                        false,
-                        caves,
-                        node,
-                        new_path_with(node, &path[..]),
-                    );
+    caves.get(current).unwrap().iter().fold(0, |count, &node| {
+        match node {
+            Node::Start => return count,
+            Node::End => return count + 1,
+            Node::Small(_) => {
+                if path.contains(&&node) {
+                    if twice {
+                        return count
+                            + traverse_cave(
+                                false,
+                                caves,
+                                &node,
+                                new_path_with(&node, &path[..]),
+                            );
+                    }
+                    return count;
+                }
             }
-            return count;
+            _ => (),
         }
 
-        count + traverse_cave(twice, caves, node, new_path_with(node, &path[..]))
+        count
+            + traverse_cave(
+                twice,
+                caves,
+                &node,
+                new_path_with(&node, &path[..]),
+            )
     })
 }
 
 #[inline]
-fn is_lower(id: Id) -> bool {
-    id.chars().all(|c| c.is_ascii_lowercase())
-}
-
-#[inline]
-fn new_path_with<'a>(node: Id<'a>, path: CavePathRef<'a>) -> CavePath<'a> {
+fn new_path_with<'a>(node: &'a Node, path: CavePathRef<'a>) -> CavePath<'a> {
     let mut new_path = path.to_vec();
     new_path.push(node);
     new_path
